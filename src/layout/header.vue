08b79c885a26a84cae20bc5b8de09d1b5450dbd9
David Costa <davidcostadev@gmail.com>
2017-07-23 14:03:48 -0300

    Adding navbar and change directory files

--- src/layout/header.vue
+++ src/layout/header.vue
@@ -1,42 +1,46 @@
 <template>
-  <header id="header-page-one" :class="navbarClass">
-    <div class="container">
-      <div class="row align-items-center">
-        <div class="col col-md-4 brand">
-          <a class="brand-link" href="/">
-            <img v-if="!detect.isMobile && !detect.isTable" src="~assets/newcommerce-h.svg" alt="newcommerce">
-            <img v-else src="~assets/newcommerce-h-inverse.svg" alt="newcommerce">
-          </a>
-        </div>
-        <div v-if="detect.isDesktop" class="col">
-          <search-top></search-top>
-        </div>
-        <div class="col col-md-8 col-lg-3 menu menu-right">
-          <div  v-if="detect.isDesktop" class="menu-item">
-            <a class="menu-link" href="#">Meus Pedidos</a>
+  <div id="header-page-one">
+    <header id="header-super" :class="navbarClass">
+      <div class="container">
+        <div class="row align-items-center">
+          <div class="col col-md-4 brand">
+            <a class="brand-link" href="/">
+              <img v-if="!detect.isMobile && !detect.isTable" src="~assets/img/newcommerce-h.svg" alt="newcommerce">
+              <img v-else src="~assets/img/newcommerce-h-inverse.svg" alt="newcommerce">
+            </a>
           </div>
-          <div v-if="!detect.isDesktop && detect.width > 440" class="menu-item">
-            <button class="menu-button" href="#"><i class="ion-ios-search"></i></button>
+          <div v-if="detect.isDesktop" class="col">
+            <search-top></search-top>
           </div>
-          <div class="menu-item cart-item">
-            <a class="menu-link" href="#"><i class="ion-ios-cart-outline"></i> <span class="badge">1</span></a>
-          </div>
-          <div v-if="!detect.isDesktop" class="menu-item">
-            <button class="menu-link" @click="openMenu"><i class="ion-navicon"></i></button>
+          <div class="col col-md-8 col-lg-3 menu menu-right">
+            <div  v-if="detect.isDesktop" class="menu-item">
+              <a class="menu-link" href="#">Meus Pedidos</a>
+            </div>
+            <div v-if="!detect.isDesktop && detect.width > 440" class="menu-item">
+              <button class="menu-button" href="#"><i class="ion-ios-search"></i></button>
+            </div>
+            <div class="menu-item cart-item">
+              <a class="menu-link" href="#"><i class="ion-ios-cart-outline"></i> <span class="badge">1</span></a>
+            </div>
+            <div v-if="!detect.isDesktop" class="menu-item">
+              <button class="menu-link" @click="openMenu"><i class="ion-navicon"></i></button>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-     <menu-mobile></menu-mobile>
-  </header>
+    </header>
+    <menu-mobile></menu-mobile>
+    <navbar v-if="detect.isDesktop"></navbar>
+  </div>
 </template>
 
 <script>
   import SearchTop from '~layout/search-top'
   import MenuMobile from '~layout/menu-mobile'
+  import Navbar from '~layout/navbar'
 
   export default {
-    components: { SearchTop, MenuMobile },
+    components: { SearchTop, MenuMobile, Navbar },
     created () {
       console.log(this.$detect.width)
     },
