9f523c346922c78a04914b8f3b6ac18c69cd5bf1
David Costa <davidcostadev@gmail.com>
2017-11-17 14:11:24 -0300

    rename files jsx -> js

--- src/components/BannerSeparate.js
+++ src/components/BannerSeparate.js
@@ -1,24 +1,28 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import Link from 'next/link';
 import styles from '../assets/scss/App.scss';
 
 const BannerSeparate = () => (
   <div className={styles.bannerSeparateGrid}>
     <div className={styles.bannerSeparateCol}>
       <div className={styles.bannerSeparateRow}>
-        <div className={`${styles.bannerSeparate} ${styles.bannerSeparateRight}`} style={{ backgroundImage: 'url(\'/img/camera.jpg\')' }}>
+        <div className={`${styles.bannerSeparate} ${styles.bannerSeparateRight}`} style={{ backgroundImage: 'url(\'/static/img/camera.jpg\')' }}>
           <span className={styles.bannerText1}>Limpador de Tela</span>
           <span className={styles.bannerText2}>R$ 17,90</span>
-          <Link to="/product" className={styles.bannerBtn}>Ver Detalhes</Link>
+          <Link href="/product">
+            <a className={styles.bannerBtn}>Ver Detalhes</a>
+          </Link>
         </div>
       </div>
     </div>
     <div className={styles.bannerSeparateCol}>
       <div className={styles.bannerSeparateRow}>
-        <div className={`${styles.bannerSeparate} ${styles.bannerSeparateRight}`} style={{ backgroundImage: 'url(\'/img/mouse.jpg\')' }}>
+        <div className={`${styles.bannerSeparate} ${styles.bannerSeparateRight}`} style={{ backgroundImage: 'url(\'/static/img/mouse.jpg\')' }}>
           <span className={styles.bannerText1}>Mouse Mac</span>
           <span className={styles.bannerText2}>R$ 227,90</span>
-          <Link to="/product" className={styles.bannerBtn}>Ver Detalhes</Link>
+          <Link href="/product">
+            <a className={styles.bannerBtn}>Ver Detalhes</a>
+          </Link>
         </div>
       </div>
     </div>
