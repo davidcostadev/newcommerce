112e2253380449ce59e16b0fc4053e299a5b1953
David Costa <davidcostadev@gmail.com>
2017-11-22 21:27:28 -0300

    Test ApiSearch

--- tests/api/Search.test.js
+++ tests/api/Search.test.js
@@ -2,12 +2,25 @@
 import ApiSearch from '../../api/Search'
 
 
-describe('Chatbot', () => {
-  it('returns data when sendMessage is called', (done) => {
+describe('ApiChat', () => {
+  it('Data notebook', (done) => {
     try {
       ApiSearch({ search: 'notebook' }).then((response) => {
         expect(response).toHaveProperty('products')
         expect(response).toHaveProperty('pagination')
+        done()
+      })
+    } catch (e) {
+      expect(e).toMatch('error')
+    }
+  })
+
+  it('Data empty', (done) => {
+    try {
+      ApiSearch({ search: 'asd' }).then((response) => {
+        expect(response).toHaveProperty('products')
+        expect(response).toHaveProperty('pagination')
+        expect(response.pagination.total).toBe(0)
         done()
       })
     } catch (e) {
