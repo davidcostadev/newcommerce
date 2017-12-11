4de6e1840c00e262357804709553ecc14bdc0f75
David Costa <davidcostadev@gmail.com>
2017-12-11 20:54:18 -0300

    Usando o title com styled

--- components/TitleSection.js
+++ components/TitleSection.js
@@ -1,12 +1,24 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import styled from 'styled-components'
 
-import styles from '../assets/scss/App.scss'
+const Section = styled.div`
+  background-image: url('https://images.unsplash.com/photo-1497171156029-51dfc973e5f9?dpr=1&auto=format&fit=crop&w=1500&q=60&cs=tinysrgb');
+  background-size: cover;
+  background-position: center;
+  padding: 30px 60px;
+  margin-bottom: 20px;
+
+`
+const Title = styled.h1`
+  color: rgb(0, 255, 255);
+  mix-blend-mode: difference;
+`
 
 const TitleSection = ({ title }) => (
-  <div className={styles.titleSection}>
-    <h1>{title}</h1>
-  </div>
+  <Section>
+    <Title>{title}</Title>
+  </Section>
 )
 
 TitleSection.propTypes = {
