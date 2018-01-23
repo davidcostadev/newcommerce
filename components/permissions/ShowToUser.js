99fe13391553c7eb9bf3d2d2e7be44d5cc459d7e
David Costa <davidcostadev@gmail.com>
2018-01-23 20:22:59 -0300

    Adding components to show only guest and user

---
+++ components/permissions/ShowToUser.js
@@ -0,0 +1,26 @@
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+
+const ShowToGuest = ({ authentication, children }) => {
+  if (!authentication) {
+    return null
+  }
+
+  return children
+}
+
+ShowToGuest.propTypes = {
+  authentication: PropTypes.bool,
+  children: PropTypes.any.isRequired,
+}
+
+ShowToGuest.defaultProps = {
+  authentication: false,
+}
+
+const mapStateToProps = ({ authentication }) => ({
+  authentication,
+})
+
+
+export default connect(mapStateToProps)(ShowToGuest)
