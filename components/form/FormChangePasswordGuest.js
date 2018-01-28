92d1050a1baa1df60a676a4fdc40634118b6ad61
David Costa <davidcostadev@gmail.com>
2018-01-28 13:23:31 -0300

    Add form to change password guest user

---
+++ components/form/FormChangePasswordGuest.js
@@ -0,0 +1,123 @@
+import React from 'react'
+// import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+// import PropTypes from 'prop-types'
+import axios from 'axios'
+// import jsCookie from 'js-cookie'
+import shortid from 'shortid'
+// import Router from 'next/router'
+import FormGroup from '../form/FormGroup'
+import Input from '../form/Input'
+import User from '../../api/User'
+// import { setAuthentication } from '../../flux/user/actions'
+import { FlashMessages } from '../page/FlashMessage'
+
+class FormChangePasswordGuest extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.save = this.save.bind(this)
+    this.handle = this.handle.bind(this)
+
+    this.state = {
+      email: '',
+      isLoading: false,
+      success: false,
+      errors: [],
+    }
+  }
+
+  handle({ target }) {
+    this.setState({
+      [target.id]: target.value,
+    })
+  }
+
+  async save(e) {
+    e.preventDefault()
+
+    // this.props.addFlashMessage({
+    //   type: 'success',
+    //   message: 'foi uma alerta',
+    // })
+
+    const { email } = this.state
+
+    this.setState({ isLoading: true })
+
+    if (email.length) {
+      try {
+        const env = {
+          PASSKEY: process.env.PASSKEY,
+          DOMAIN_API: process.env.DOMAIN_API,
+        }
+
+        await User.resetPassword(env, axios.post, email)
+        this.setState({ success: true })
+      } catch (error) {
+        console.error(error)
+
+        this.addErrors({
+          type: 'danger',
+          message: 'Não foi possivel fazer o resetar sua senha',
+        })
+      }
+    } else {
+      this.addErrors({
+        type: 'danger',
+        message: 'Por favor preencha seu email',
+      })
+    }
+    this.setState({ isLoading: false })
+  }
+
+  addErrors({ type, message }) {
+    this.setState({
+      errors: [
+        {
+          id: shortid.generate(),
+          type,
+          message,
+        },
+      ],
+    })
+  }
+
+  render() {
+    const { isLoading, errors, success } = this.state
+
+    if (success) {
+      const message = [
+        {
+          type: 'success',
+          message: 'Um email foi encaminhado para sua endereço de email e siga as instrução contida nele.',
+        },
+      ]
+
+      return (
+        <FlashMessages msgs={message} />
+      )
+    }
+
+    return (
+      <form className="form" onSubmit={this.save}>
+        <FlashMessages msgs={errors} />
+        <FormGroup>
+          <label htmlFor="email" className="control-label">Email</label>
+          <Input
+            id="email"
+            label="Email"
+            placeholder="Seu email"
+            onChange={this.handle}
+          />
+        </FormGroup>
+        <div className="text-center">
+          <button className="btn btn-primary" disabled={isLoading}>Resetar</button>
+        </div>
+      </form>
+    )
+  }
+}
+
+
+export default connect()(FormChangePasswordGuest)
--- pages/password.js
+++ pages/password.js
@@ -1,9 +1,49 @@
 import React from 'react'
+import withRedux from 'next-redux-wrapper'
+import Page from '../containers/PageHOF'
+import { initStore } from '../store'
+import { Container, PageContent } from '../layout/Pages'
+import FormChangePasswordGuest from '../components/form/FormChangePasswordGuest'
 
-const Order = () => (
-  <div>
-    Password
-  </div>
-)
+class Password extends React.Component {
+  static async getInitialProps({ req, store, isServer }) {
+    const { sessionId } = await Page.getInitialProps(store, req, isServer)
 
-export default Order
+    const urlMeta = {
+      PS_TITLE: 'password',
+      PS_DESCRIPTION: 'If you dont remember anymore of your password, you can reset it in this page',
+    }
+
+    return {
+      sessionId,
+      urlMeta,
+    }
+  }
+
+  render() {
+    return (
+      <Page {...this.props}>
+        <Container>
+          <PageContent>
+            <div className="row justify-content-sm-center">
+              <div className="col-sm-8">
+                <h1>Resetar Senha</h1>
+                <p>Seguida os passos abaixo:</p>
+                <ul>
+                  <li>Digite seu email</li>
+                  <li>Uma mensagem será enviada</li>
+                  <li>Siga a instruções contidas na mensagem</li>
+                </ul>
+                <FormChangePasswordGuest />
+              </div>
+            </div>
+          </PageContent>
+        </Container>
+      </Page>
+    )
+  }
+}
+
+const mapState = state => state
+
+export default withRedux(initStore, mapState)(Password)
