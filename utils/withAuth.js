032762eeacd211e2570eb297a8fed5cef023b286
David Costa <davidcostadev@gmail.com>
2018-02-14 10:17:14 -0300

    Removing code unuseless

--- utils/withAuth.js
+++ utils/withAuth.js
@@ -4,7 +4,6 @@ import PropTypes from 'prop-types'
 import { authSession, redirect } from './user'
 import isRedirect from '../utils/auth'
 
-
 const withAuth = (ComposedComponent) => {
   class Auth extends React.Component {
     static async getInitialProps(context) {
@@ -18,21 +17,13 @@ const withAuth = (ComposedComponent) => {
       if (typeof ComposedComponent.auth !== 'undefined') {
         auth = ComposedComponent.auth()
       }
-
       if (isRedirect(loggedIn, auth.rules)) {
-        // console.log('redirect', true)
         redirect(context, '/')
-      } else {
-        // console.log('redirect', false)
       }
 
       const props = await ComposedComponent.getInitialProps(context)
 
       return props
-    }
-
-    componentWillMount() {
-      // jsCookie.set('logged', true, { expires: 7 })
     }
 
     render() {
@@ -54,9 +45,7 @@ const withAuth = (ComposedComponent) => {
     user,
   })
 
-
   return connect(mapStateToProps)(Auth)
 }
-
 
 export default withAuth
