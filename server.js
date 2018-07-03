ca43f5410813766ad3e75de7f98340fe50a09ff0
David Costa <davidcostadev@gmail.com>
2018-07-03 14:27:00 -0300

    Add rollbar

--- server.js
+++ server.js
@@ -3,11 +3,14 @@ const next = require('next')
 const qs = require('querystring')
 const cookieParser = require('cookie-parser')
 const routes = require('./routes')
+const Rollbar = require('rollbar')
 
 require('dotenv').config()
 
 const app = next({ dev: process.env.NODE_ENV !== 'production' })
 const handler = routes.getRequestHandler(app)
+
+const rollbar = new Rollbar(process.env.ROLLBAR_TOKEN)
 
 // With express
 const express = require('express')
@@ -24,6 +27,7 @@ app.prepare().then(() => {
       saveUninitialized: false,
       cookie: { maxAge: 60 * 60 * 24 * 7 * 1000 },
     }))
+    .use(rollbar.errorHandler())
 
 
   server.get('/redefinir-senha', (req, res) => {
